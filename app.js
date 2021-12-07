2f481e3a8b76841a174c5331349caa8581b6cec7
tuosm9390 <tuosm123@naver.com>
2021-12-07 16:49:09 +0900

    Add root path routh handler

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const { Member } = db;
 
 app.use(express.json()); //middleware
 
+app.get('/', (req, res) => {
+    res.send('URL should contain /api/..');
+});
+
 app.get('/api/members', async (req, res) => {
     const { team } = req.query;
     if ( team ) {
